1ee6ea409ff42101bd106a1b77b3a559d80ee662
thiha95 <42903979+thiha95@users.noreply.github.com>
2020-08-11 14:14:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,13 +10,55 @@ import pandas as pd
 
 ########### Define your variables
 
-tabtitle='A ProQuest TDM Project'
+tabtitle='PQTDM'
 myheading='CBS 60 Miniutes Analysis'
 
 ########### Set up the chart
 
+# fig2
+
+# polarity scatter plot
+
+fig2 = go.Figure()
+
+df_pol = pd.read_csv('df_pol.csv')
+
+# make subplots
+fig2.add_trace(
+    go.Scatter(
+        x=df_pol[df_pol['polarity'] >= 0][df_pol['word_count'] >= 100]['year'],
+        y=df_pol[df_pol['polarity'] >= 0][df_pol['word_count'] >= 100]['polarity'],
+        mode="markers",
+        marker=dict(color='red')
+    ))
+
+fig2.add_trace(
+    go.Scatter(
+        x=df_pol[df_pol['polarity'] < 0][df_pol['word_count'] >= 100]['year'],
+        y=df_pol[df_pol['polarity'] < 0][df_pol['word_count'] >= 100]['polarity'],
+        mode="markers",
+        marker=dict(color='blue')
+    ))
 
 
+fig2.update_layout(
+    autosize=False,
+    showlegend=False,
+    plot_bgcolor='white'
+)
+
+annotations = []
+
+# Title
+annotations.append(dict(xref='paper', yref='paper', x=0.0, y=1.05,
+                              xanchor='left', yanchor='bottom',
+                              text='Style: Positive vs Negative',
+                              font=dict(family='Arial',
+                                        size=24,
+                                        color='rgb(37,37,37)'),
+                              showarrow=False))
+
+fig2.update_layout(annotations=annotations)
 
 
 ########### Initiate the app
@@ -27,7 +69,15 @@ app.title=tabtitle
 
 ########### Set up the layout
 app.layout = html.Div(children=[
-    html.H1(myheading)
+    html.H1(myheading),
+    
+    html.Div(children='''
+        A ProQuest TDM Project.
+    '''),
+    
+    html.Div(dcc.Graph(
+        id='example-graph2',
+        figure=fig2))
     ]
 )
 
