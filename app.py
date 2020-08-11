77d93e963d592ced595cbcd2ffec5d64e8e28c20
thiha95 <42903979+thiha95@users.noreply.github.com>
2020-08-11 13:46:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,61 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-
-import plotly.graph_objects as go
-import numpy as np
-import plotly.express as px
-import pandas as pd
-
-# fig 0
-
-bigram_df = pd.read_csv('https://raw.githubusercontent.com/thiha95/flying-dog-beers/master/bigram_df.csv', error_bad_lines=False)
-
-fig0 = px.bar(bigram_df, y="bigram", x="freq", color = 'freq', animation_frame="year", orientation = 'h',
-
-             title="Key Phrases of the Year",
-
-             labels={ # replaces default labels by column name
-                "bigram": "Two-Word Phrases",  "freq": "Frequency"},
-             color_continuous_scale=px.colors.sequential.Viridis,
-             template="simple_white")
+import plotly.graph_objs as go
 
 
+########### Define your variables
+beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
+ibu_values=[35, 60, 85, 75]
+abv_values=[5.4, 7.1, 9.2, 4.3]
+color1='lightblue'
+color2='darkgreen'
+mytitle='Beer Comparison'
+tabtitle='beer!'
+myheading='Flying Dog Beers'
+label1='IBU'
+label2='ABV'
+githublink='https://github.com/austinlasseter/flying-dog-beers'
+sourceurl='https://www.flyingdog.com/beers/'
+
+########### Set up the chart
+bitterness = go.Bar(
+    x=beers,
+    y=ibu_values,
+    name=label1,
+    marker={'color':color1}
+)
+alcohol = go.Bar(
+    x=beers,
+    y=abv_values,
+    name=label2,
+    marker={'color':color2}
+)
+
+beer_data = [bitterness, alcohol]
+beer_layout = go.Layout(
+    barmode='group',
+    title = mytitle
+)
+
+beer_fig = go.Figure(data=beer_data, layout=beer_layout)
+
+
+########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
-app.title= 'ProQuest TDM: A Sample'
+app.title=tabtitle
 
-
+########### Set up the layout
 app.layout = html.Div(children=[
-    html.H1(children='CBS 60 Minutes'),
-
-    html.Div(children='''
-        A ProQuest TDM Project.
-    '''),
-
-    html.Div(dcc.Graph(
-        id='example-graph',
-        figure=fig0
-    ))
-        ])
+    html.H1(myheading),
+    dcc.Graph(
+        id='flyingdog',
+        figure=beer_fig
+    )
+    ]
+)
 
 if __name__ == '__main__':
     app.run_server()
